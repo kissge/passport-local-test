6c601e805474154ecdee1b2fd2a0e9b2b1a15369
Yusuke Kido <yusuke.kido@legalscape.co.jp>
2019-02-02 07:10:02 +0000

    Add user module

---
+++ src/user/user.controller.ts
@@ -0,0 +1,7 @@
+import { Get, Controller } from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Controller()
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,11 @@
+import { HttpException, Injectable, Inject } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+}
